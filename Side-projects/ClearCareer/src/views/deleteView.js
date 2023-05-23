5b912abef7a29580ca137089072e4ffdecb534af
Bogomil Simitchiev <simitchiev365@abv.bg>
2023-05-24 01:53:13 +0300

    Changed question

--- Side-projects/ClearCareer/src/views/deleteView.js
+++ Side-projects/ClearCareer/src/views/deleteView.js
@@ -1,7 +1,7 @@
 import { deleteOffer } from '../services/requests.js';
 
 export const deleteView = (ctx) => {
-    if (confirm("Do you want to delete this album?") == true) {
+    if (confirm("Do you want to delete this offer?") == true) {
         deleteOffer(ctx.params.id);
     }
 }
